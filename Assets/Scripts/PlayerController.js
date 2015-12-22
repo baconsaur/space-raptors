3f998f94d3c7d2999deb52d375ce827d68cdca9d
Ash Kestrel <tetrapteryxgames@gmail.com>
2015-12-22 11:19:26 -0700

    :shoe: Stegs walk cycle and animator set up

--- Assets/Scripts/PlayerController.js
+++ Assets/Scripts/PlayerController.js
@@ -5,6 +5,7 @@ var jumpForce :float;
 var rigidBody :Rigidbody2D;
 var maxVelocity :float;
 var turnDeadZone :float;
+var animator :Animator;
 
 function Start () {
 
@@ -12,14 +13,29 @@ function Start () {
 
 function FixedUpdate () {
 	var direction = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
-	Debug.Log(direction);
+	transform.Translate(new Vector2(direction, 0));
+	
+	if (direction != 0) {
+		animator.SetBool("walking", true);
+	} else {
+		animator.SetBool("walking", false);
+	}
+	
 	if (direction > turnDeadZone) {
 		transform.localScale = new Vector3(1, 1, 1);
+		animator.SetBool("walking", true);
 	} else if (direction < -turnDeadZone) {
 		transform.localScale = new Vector3(-1, 1, 1);
+		animator.SetBool("walking", true);
 	}
-	transform.Translate(new Vector2(direction, 0));
+	
 	if (Input.GetAxis("Vertical") > 0 && rigidBody.velocity.y == 0) {
 		rigidBody.AddForce(Vector2(0, jumpForce));
+	}
+	
+	if (rigidBody.velocity.y != 0) {
+		animator.SetBool("jumping", true);
+	} else {
+		animator.SetBool("jumping", false);
 	}
 }
