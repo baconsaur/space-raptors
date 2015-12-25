4936a513f9b345e1f465d9659daa4c591876a94c
Ben Allred <skuttleman@gmail.com>
2015-12-24 20:23:39 -0700

    Almost testable

--- Assets/Scripts/bda-test-ai.js
+++ Assets/Scripts/bda-test-ai.js
@@ -22,9 +22,9 @@ public class Pointers {
 	var animator :Animator;
 	var player :GameObject;
 }
-var capabilities :Capabilities;
-var reactions :Reactions;
-var pointers :Pointers;
+public var capabilities :Capabilities;
+public var reactions :Reactions;
+public var pointers :Pointers;
 
 
 // Script Variables
@@ -33,6 +33,118 @@ private var meleeCoolDown :float = 1f;
 private var reactionCoolDown :float = 1f;
 private var ready :boolean = true;
 private var turnable :boolean = true;
+private var lastLocation :GameObject;
+
+
+
+// Muscle-Man Action Instructions Class
+private class Instruction {
+	private var action :Function;
+	private var argument :float;
+	public function Instruction(act :Function, arg :float) {
+		action = act;
+		argument = arg;
+	}
+	public function go() {
+		action(argument);
+	}
+	
+	static function GetToFrom(start :GameObject, end :GameObject) :Instruction {
+		var jumpRatio = 1f;
+		var startCorners = new Corners(start);
+		var endCorners = new Corners(end);
+		var argument :float;
+		if (true) {
+			return new Instruction(PathActions.JumpUpLeft, argument);
+		} else if (true) {
+			return new Instruction(PathActions.JumpUpRight, argument);
+		} else if (true) {
+			return new Instruction(PathActions.JumpAcrossLeft, argument);
+		} else if (true) {
+			return new Instruction(PathActions.JumpAcrossRight, argument);
+		} else if (true) {
+			return new Instruction(PathActions.JumpAroundLeft, argument);
+		} else if (true) {
+			return new Instruction(PathActions.JumpAroundRight, argument);
+		} else if (true) {
+			return new Instruction(PathActions.GoLeft, argument);
+		} else if (true) {
+			return new Instruction(PathActions.GoRight, argument);
+		} else if (true) {
+			return new Instruction(PathActions.FallAroundLeft, argument);
+		} else if (true) {
+			return new Instruction(PathActions.FallAroundRightt, argument);
+		}
+		else return null;
+	}
+	
+	static function GetPossibleUpPath(start :GameObject, end :GameObject, platforms :GameObject[]) :Array {
+		if (start == end) return new Array(GetToFrom(start, end));
+		for (var i :int = 0; i < platforms.length; i++) {
+			if (platforms[i] != start) {
+				var instruction :Instruction = Instruction.GetToFrom(start, platforms[i]);
+				if (instruction) {
+					var works :Array = Instruction.GetPossibleUpPath(platforms[i], end, platforms);
+					if (works) return new Array(instruction).concat(works);
+				}
+			}
+		}
+		return null;
+	}
+}
+
+private class PathActions {
+	static function JumpUpLeft(distance :float) {
+	
+	}
+	static function JumpUpRight(distance :float) {
+	
+	}
+	static function JumpAcrossLeft(distance :float) {
+	
+	}
+	static function JumpAcrossRight(distance :float) {
+	
+	}
+	static function JumpAroundLeft(distance :float) {
+	
+	}
+	static function JumpAroundRight(distance :float) {
+	
+	}
+	static function GoLeft(distance :float) {
+	
+	}
+	static function GoRight(distance :float) {
+	
+	}
+	static function FallAroundLeft(distance :float) {
+		
+	}
+	static function FallAroundRightt(distance :float) {
+		
+	}
+}
+
+// Class for finding boxcollider corners
+public class Corners {
+	public var topLeft :Vector2;
+	public var topRight :Vector2;
+	public var bottomLeft :Vector2;
+	public var bottomRight :Vector2;
+	public function Corners(object :GameObject) {
+		var pos :Vector2 = new Vector2 (object.transform.position.x, object.transform.position.y);
+		var collider :BoxCollider2D = object.GetComponent(BoxCollider2D);
+		var ext :Vector2 = new Vector2(collider.bounds.extents.x, collider.bounds.extents.y);
+		var off :Vector2 = collider.offset;
+		topLeft = new Vector2(pos.x - ext.x + off.x, pos.y + ext.y + off.y);
+		topRight = new Vector2(pos.x + ext.x + off.x, pos.y + ext.y + off.y);
+		bottomLeft = new Vector2(pos.x - ext.x + off.x, pos.y - ext.y + off.y);
+		bottomRight = new Vector2(pos.x + ext.x + off.x, pos.y - ext.y + off.y);
+	}
+}
+
+
 
 
 
@@ -62,11 +174,13 @@ function FixedUpdate () {
 				
 				var myPlatform :GameObject = onPlatform(pointers.player, 0.1);
 				var playerPlatform :GameObject = onPlatform(pointers.player, 0.1);
-				if (myPlatform == playerPlatform || !myPlatform || !playerPlatform) {
+				// if player.platform = self.platform - move to player
+				if (myPlatform == playerPlatform || !myPlatform || !playerPlatform || (playerPlatform && lastLocation && playerPlatform != lastLocation)) {
+					if (playerPlatform) lastLocation = playerPlatform;
 					var moveAmount = -transform.localScale.x * capabilities.speed * Time.deltaTime;
 					transform.Translate(new Vector2(moveAmount, 0));
 				}
-				// if player.platform = self.platform - move to player
+				
 				
 				// recursive calculate platform path to player to build move instructions
 				// follow instructions while player is on same platform or in the air
@@ -84,7 +198,6 @@ function distanceCalc(point1 :Vector2, point2 :Vector2): float {
 function FacePlayer() {
 	var scale :float = -xScale(0.1);
 	if (scale) transform.localScale = new Vector3(scale, 1f, 1f);
-	else Debug.Log(scale);
 }
 
 function xScale(tolerance :float) :float {
