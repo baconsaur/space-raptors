6d4bc3d14bfbc516c8c657c1174c73f94c4d76de
Ash Kestrel <tetrapteryxgames@gmail.com>
2015-12-26 17:08:32 -0700

    :boom: Player can shoot in both directions

--- Assets/Scripts/PlayerController.js
+++ Assets/Scripts/PlayerController.js
@@ -6,21 +6,30 @@ var rigidBody :Rigidbody2D;
 var maxVelocity :float;
 var turnDeadZone :float;
 var animator :Animator;
+var shotOffset :Vector2;
+var currentWeapon :GameObject;
+var defaultCooldown : int;
+private var shotCooldown :int;
 
 function Start () {
-
+	shotCooldown = 0;
 }
 
 function FixedUpdate () {
+	if (shotCooldown) {
+		shotCooldown--;
+	}
 	var direction = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
 	transform.Translate(new Vector2(direction, 0));
 	
 	if (direction > turnDeadZone) {
 		transform.localScale = new Vector3(1, 1, 1);
 		animator.SetBool("walking", true);
+		shotOffset = Vector2(2, .02);
 	} else if (direction < -turnDeadZone) {
 		transform.localScale = new Vector3(-1, 1, 1);
 		animator.SetBool("walking", true);
+		shotOffset = Vector2(-2, -.02);
 	} else {
 		animator.SetBool("walking", false);
 	}
@@ -32,5 +41,13 @@ function FixedUpdate () {
 	} else if (rigidBody.velocity.y == 0) {
 	//TODO: This also gets set to 0 at the apex of a jump
 		animator.SetBool("jumping", false);
+	}
+	
+	var shoot = Input.GetAxis("Fire1");
+	if (!shotCooldown && shoot) {
+	animator.SetTrigger("shoot");
+		var newShot = Instantiate(currentWeapon, Vector2(gameObject.transform.position.x + shotOffset.x, gameObject.transform.position.y + shotOffset.y), Quaternion.identity);
+		newShot.GetComponent(ProjectileController).direction = transform.localScale.x;
+		shotCooldown = defaultCooldown;
 	}
 }
---
+++ Assets/Scripts/ProjectileController.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var speed :float;
+var animator: Animator;
+var direction :int;
+
+function Start () {
+	transform.localScale = new Vector3(direction, 1, 1);
+}
+
+function Update () {
+	if (animator.GetBool("hit") == false) {
+		transform.Translate(speed * direction * Time.deltaTime, 0, 0);
+	}
+	if (transform.position.x > 10) {
+		Destroy(gameObject);
+	}
+}
+
+function OnCollisionEnter2D() {
+	animator.SetBool("hit", true);
+}
+
+function DestroyThis() {
+	Destroy(gameObject);
+}
