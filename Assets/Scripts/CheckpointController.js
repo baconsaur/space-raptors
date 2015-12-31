1154c88d968453ac9daf22dbd6baea1f642dde45
Ben Allred <skuttleman@gmail.com>
2015-12-31 12:05:38 -0700

    Checkpoint skeleton

---
+++ Assets/Scripts/CheckpointController.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+public class CheckpointController extends MonoBehaviour {
+	public static var checkpoint :CheckpointController;
+
+	public var playerHealth :int;
+
+
+	private var player :GameObject;
+	private var playerController :PlayerController;
+
+
+
+	function Awake() {
+		if (!checkpoint) {
+			DontDestroyOnLoad(this.gameObject);
+			checkpoint = this;
+		} else if (checkpoint != this) {
+			Destroy(this.gameObject);
+		}
+		player = GameObject.Find('Player');
+		playerController = player.GetComponent(PlayerController);
+	}
+
+	public function Save() {
+		playerHealth = playerController.health;
+	}
+}
