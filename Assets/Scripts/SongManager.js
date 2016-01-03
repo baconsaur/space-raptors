196de23cab26917b5e8ee88e8924a46d57fb4175
Ben Allred <skuttleman@gmail.com>
2016-01-03 07:55:30 -0700

    MUSIC: added boss music and music switches in main scene between two levels

--- Assets/Scripts/SongManager.js
+++ Assets/Scripts/SongManager.js
@@ -2,10 +2,12 @@
 
 var song1 :AudioSource;
 var song2 :AudioSource;
+var song3 :AudioSource;
 var dropDown :UnityEngine.UI.Dropdown;
 
 function Start () {
-	song2.Play();
+	if (Application.loadedLevelName == 'MiniBossFight') Play(3);
+	else Play(2);
 }
 
 function Update () {
@@ -13,16 +15,19 @@ function Update () {
 }
 
 function Play(num :int) {
-	num = dropDown.value;
+//	num = dropDown.value;
 	StopAll();
 	if(num == 1) {
 		song1.Play();
-	} else if (num ==2) {
+	} else if (num == 2) {
 		song2.Play();
+	} else if (num == 3) {
+		song3.Play();
 	}
 }
 
 function StopAll() {
 	song1.Stop();
 	song2.Stop();
+	song3.Stop();
 }
