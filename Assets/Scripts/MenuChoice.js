39cae9770d3e59c4a27e71b0c2a4e4e8876549c3
Ash T. Kestrel <tetrapteryxgames@gmail.com>
2016-01-03 18:46:08 -0700

    :tada: Main menu and intro sequence

---
+++ Assets/Scripts/MenuChoice.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+function MenuChoice (item :int) {
+	if (item == 1) {
+		SceneManagement.SceneManager.LoadScene("Backstory");
+	} else if (item == 4) {
+		Application.Quit();
+	}
+}
