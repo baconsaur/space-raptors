2eb3e0b8c68497f24a47e7926dc995e5c18e7591
Ash T. Kestrel <tetrapteryxgames@gmail.com>
2016-01-03 19:21:15 -0700

    :clock1: Pause menu

---
+++ Assets/Scripts/PauseMenu.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+private var player :GameObject;
+
+function Start () {
+	Time.timeScale = 0;
+	player = GameObject.Find("Player");
+}
+
+function MenuChoice (item :int) {
+	if (item == 1) {
+		Time.timeScale = 1;
+		player.GetComponent(PlayerController).paused = false;
+		Destroy(gameObject);
+	} else if (item == 3) {
+		SceneManagement.SceneManager.LoadScene("Menu");
+	} else if (item == 4) {
+		Application.Quit();
+	}
+}
