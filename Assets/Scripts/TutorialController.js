cb4fba25dcd16e46373f0469aba42769cee3d164
Ash Kestrel <tetrapteryxgames@gmail.com>
2016-02-13 17:38:42 -0700

    Added tutorial

---
+++ Assets/Scripts/TutorialController.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+var tutorialHUD : GameObject;
+var nextOverlay : GameObject;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter2D (collision : Collider2D) {
+	if (collision.gameObject.name == "Player") {
+		Methods.destroyChildren(tutorialHUD);
+		var newOverlay = Instantiate(nextOverlay);
+		newOverlay.transform.parent = tutorialHUD.transform;
+		newOverlay.transform.localPosition = Vector3(0, 0, 0);
+		Destroy(this.gameObject);
+	}
+}
